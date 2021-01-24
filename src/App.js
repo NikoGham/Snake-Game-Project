0bce15ccb3fb08b0eaf72b0c10075f798c5edff2
Niko Gham <ng@nickgillham.co.uk>
2021-01-24 20:55:11 +0000

    edits to functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,76 @@
 import './App.css'
-import {useState} from 'react'
-import Box from './components/activeItems/box'
+import { useState } from 'react'
+import Box from './components/activeItems/Box'
 
 function App() {
+	// State for position of box
+	const [ position, setPosition ] = useState({
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0
+	})
 
-  // State for position of box
-  const [ position, setPosition ] = useState({
-    top: '', 
-    bottom: '', 
-    left: '',
-    right:''
-  })
+	// Destructuring
+	const { top, bottom, left, right } = position
 
-  // Destructuring 
-  const { top, bottom, left, right } = position
+	// test to move item
+	const moveDown = (e) => {
+		setPosition({ ...position, top: position.top + 50 })
+	}
 
-  // test to move item
-  const moveUp = e => {
-    alert('Hello World')
-  } 
+	const moveUp = (e) => {
+		setPosition({ ...position, top: position.top - 50 })
+	}
+	const moveLeft = (e) => {
+		setPosition({ ...position, left: position.left - 50 })
+	}
+	const moveRight = (e) => {
+		setPosition({ ...position, left: position.left + 50 })
+	}
+
+
+	let detectKey = (e) => {
+    switch(e.keyCode) {
+      case 40:
+        moveDown()
+        break
+      case 38:
+        moveUp()
+        break
+      case 37:
+        moveLeft()
+        break
+      case 39:
+        moveRight()
+        break
+      default:
+      break
+    }
+  }
+  
+  document.onkeydown = detectKey
+
 
 	return (
 		<div className='container'>
-			<h1 className="text-center">Snake Game</h1>
+			<h1 className='text-center'>Snake Game</h1>
 
 			<Box top={top} bottom={bottom} left={left} right={right} />
 
-      <button className="btn btn-dark" onClick={moveUp}>Add one to top</button>
+			<button className='btn btn-dark' onClick={moveDown}>
+				Move Down
+			</button>
+			<button className='btn btn-dark' onClick={moveUp}>
+				{' '}
+				Move Up{' '}
+			</button>
+			<button className='btn btn-dark' onClick={moveLeft}>
+				Move Left
+			</button>
+			<button className='btn btn-dark' onClick={moveRight}>
+				Move Right
+			</button>
 		</div>
 	)
 }
--- src/components/activeItems/box.js
+++ src/components/activeItems/box.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const box = ({}) => {
+const Box = ({top, bottom, left, right}) => {
+
+    const style = {
+        left: `${left}px`, 
+        top: `${top}px`, 
+        bottom: `${bottom}px`, 
+        right: `${right}px`, 
+        
+    }
+
+    console.log(style)
+
     return (
-        <div id="box">
+        <div style={style} className="bg-success" id="box">
             
         </div>
     )
 }
 
-export default box
+Box.propTypes = {
+    top: PropTypes.number.isRequired,
+    bottom: PropTypes.number.isRequired,
+    left: PropTypes.number.isRequired,
+    right: PropTypes.number.isRequired,
+}
+
+export default Box
